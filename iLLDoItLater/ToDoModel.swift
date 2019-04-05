3e7384697e5c73fcd8994380e3274cacefd89ab3
West Kraemer <kraemerwest@gmail.com>
2019-04-05 14:07:54 -0400

    create toDo datamodel

---
+++ iLLDoItLater/ToDoModel.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoModel.swift
+//  iLLDoItLater
+//
+//  Created by West Kraemer on 4/5/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var title: String
+    var description: String
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
--- iLLDoItLater/ViewController.swift
+++ iLLDoItLater/ViewController.swift
@@ -10,15 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
     @IBOutlet weak var titleLabel: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.titleLabel.layer.cornerRadius = 10
     }
 
+
+    
+    
+    
+    
+    
 
 }
 
