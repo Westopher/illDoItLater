9eeb5273aa6b4b9f02018d04ff7975e426088028
West Kraemer <kraemerwest@gmail.com>
2019-04-05 12:38:01 -0400

    add tableview and embed in nav controller

--- iLLDoItLater/ViewController.swift
+++ iLLDoItLater/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
