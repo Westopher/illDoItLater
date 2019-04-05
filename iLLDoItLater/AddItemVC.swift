9d42ce13cc38b9defac0b2124952c5e37fec47a4
West Kraemer <kraemerwest@gmail.com>
2019-04-05 15:26:05 -0400

    add iboutlet and actions

---
+++ iLLDoItLater/AddItemVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddItemVC.swift
+//  iLLDoItLater
+//
+//  Created by West Kraemer on 4/5/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class AddItemVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        print("pressed")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
