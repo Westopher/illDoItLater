61a9f8b0beb78afef092d24c9242cdee11e455e7
West Kraemer <kraemerwest@gmail.com>
2019-05-06 16:57:54 -0400

    add delete button styling

--- iLLDoItLater/View Controllers/DeleteAllVC.swift
+++ iLLDoItLater/View Controllers/DeleteAllVC.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import RealmSwift
 
 class DeleteAllVC: UIViewController {
 
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.deleteButton.layer.cornerRadius = 10
     }
     
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+//        let realm = try! Realm()
+//        try! realm.write {
+//        }
+        
+    }
     
     
     
