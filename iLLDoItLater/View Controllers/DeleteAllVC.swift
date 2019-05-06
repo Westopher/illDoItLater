70ff4b0f6fe37a017d0675b42ce1d8b5f9c5fb8d
West Kraemer <kraemerwest@gmail.com>
2019-05-06 16:49:32 -0400

    add vc and screen for remove items

---
+++ iLLDoItLater/View Controllers/DeleteAllVC.swift
@@ -0,0 +1,25 @@
+//
+//  DeleteAllVC.swift
+//  iLLDoItLater
+//
+//  Created by West Kraemer on 5/6/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class DeleteAllVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+    
+
+  
+
+}
