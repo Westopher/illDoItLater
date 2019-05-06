e28a2bb308d89e6ffb366adad3c7d006ad5523a7
West Kraemer <kraemerwest@gmail.com>
2019-05-06 17:54:05 -0400

    get realm objects to display on list

--- iLLDoItLater/View Controllers/AddItemVC.swift
+++ iLLDoItLater/View Controllers/AddItemVC.swift
@@ -38,6 +38,7 @@ class AddItemVC: UIViewController {
             realm.add(toDoRealmObject)
         }
         
+        
         print("add button pressed")
         
         
--- iLLDoItLater/View Controllers/ViewController.swift
+++ iLLDoItLater/View Controllers/ViewController.swift
@@ -17,40 +17,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var tableview: UITableView!
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.titleLabel.layer.cornerRadius = 10
+        getListItemsFromRealm()
         
         print(Realm.Configuration.defaultConfiguration.fileURL)
     }
+    
+    
+    func getListItemsFromRealm() {
+        let realm = try! Realm()
+        toDosArray = realm.objects(ToDoObject.self)
+        
+        tableview.reloadData()
+    }
+    
+    
 
 }
 
-    extension ViewController: UITableViewDataSource {
+    extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDosArray == nil ? 0: toDosArray!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let toDo = toDosArray?[indexPath.row]
-        
-        cell.textLabel?.text = toDo?.title
-        
+        if toDosArray != nil {
+            cell.textLabel?.text = toDosArray?[indexPath.row].title
+        }
         return cell
     }
     
+        
+        
+        
+        
+        
+        
+        
+        
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             //if the user taps on the plus sign in the upper right to add a new list item, send them to the add item view
             if segue.identifier == "AddNewSegue" {
                 let addVC = segue.destination as! AddItemVC
                 addVC.ToDoListItemsVC = self
             }
-}
-}
-            //if the user taps on a list item, this sets the next page's labels
+        }
+    }
+
+//            //if the user taps on a list item, this sets the next page's labels
 //            if segue.identifier == "DetailSegue" {
 //                if let ViewItemVC = segue.destination as? ViewToDoListItemVC,
 //                    let ip = tableview.indexPathForSelectedRow {
@@ -58,36 +80,4 @@ class ViewController: UIViewController {
 //                    ViewItemVC.descriptionToDisplay = "\(toDosArray?[ip.row].description)"
 //                }
 //            }
-//            
-//        }
-//    
-//}
-//
-//
-//extension ViewController: UITableViewDelegate {
-//    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        performSegue(withIdentifier: "DetailSegue", sender: nil)
-//    }
-//    
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//            toDosArray.remove(at: indexPath.row)
-//            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
-//        }
-//    }
-//    
-//    func tableView(_ tableView: UITableView, titleForDeleteConfirmationButtonForRowAt indexPath: IndexPath) -> String? {
-//        return "Remove"
-//    }
-//    
-//    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-//        
-//        let source = toDosArray[sourceIndexPath.row]
-//        toDosArray.remove(at: sourceIndexPath.row)
-//        toDosArray.insert(source, at: destinationIndexPath.row)
-//    }
-//    
-//}
-//
-//
+
