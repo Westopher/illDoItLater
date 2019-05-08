a03a4d9b0fcb8a3c99f27866987448e404c6292d
West Kraemer <kraemerwest@gmail.com>
2019-05-07 20:28:55 -0400

    add styling to edit button

---
+++ iLLDoItLater/View Controllers/EditVC.swift
@@ -0,0 +1,24 @@
+//
+//  EditVC.swift
+//  iLLDoItLater
+//
+//  Created by West Kraemer on 5/7/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class EditVC: UIViewController {
+
+    @IBOutlet weak var finishEditingButtonPressed: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.finishEditingButtonPressed.layer.cornerRadius = 10
+        
+    }
+    
+
+   
+
+}
