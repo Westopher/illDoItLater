ad87e251bbce57fc35153614e590e8975da2d6cf
West Kraemer <kraemerwest@gmail.com>
2019-05-22 12:08:18 -0400

    add UI for email VC. Also add email VC

---
+++ iLLDoItLater/View Controllers/SendEmailVC.swift
@@ -0,0 +1,27 @@
+//
+//  SendEmailVC.swift
+//  iLLDoItLater
+//
+//  Created by West Kraemer on 5/22/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class SendEmailVC: UIViewController {
+
+    @IBOutlet weak var sendEmailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+    @IBAction func sendEmailPressed(_ sender: Any) {
+    }
+    
+    
+
+}
